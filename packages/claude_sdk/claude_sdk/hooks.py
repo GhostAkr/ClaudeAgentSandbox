d5040993aa33c82f2f77904509ee9e7b7c889bf3
Alexander Antoshkin <ighost99@gmail.com>
2026-08-26 21:50:03 +0200

    feat: add a PostToolUse hook

--- packages/claude_sdk/claude_sdk/hooks.py
+++ packages/claude_sdk/claude_sdk/hooks.py
@@ -12,3 +12,11 @@ async def restrict_access_to_env(input_data: object, tool_use_id, context) -> ob
         }
 
     return {}
+
+async def say_goodbye(input_data: object, tool_use_id, context) -> object:
+    return {
+        "hookSpecificOutput": {
+            "hookEventName": input_data["hook_event_name"],
+            "additionalContext": "Say goodbye after writing the tool's result"
+        }
+    }
--- packages/claude_sdk/claude_sdk/main_agent.py
+++ packages/claude_sdk/claude_sdk/main_agent.py
@@ -1,6 +1,6 @@
 from claude_agent_sdk import (AssistantMessage, ClaudeAgentOptions,
                               HookMatcher, ResultMessage, query)
-from claude_sdk.hooks import restrict_access_to_env
+from claude_sdk.hooks import restrict_access_to_env, say_goodbye
 from claude_sdk.subagents import subagents
 from dotenv import load_dotenv
 
@@ -13,11 +13,12 @@ files_to_read: list(str) = [
 
 async def run_coordinator():
     async for message in query(
-        prompt=f"Read {files_to_read[1]} and summarize its contents.",
+        prompt=f"Read {files_to_read[0]} and summarize its contents.",
         options=ClaudeAgentOptions(
             # agents=subagents,  # Uncomment if you want to test subagents. Note that it might conflict with the hooks
             hooks={
-                "PreToolUse": [HookMatcher(matcher="Read|Edit", hooks=[restrict_access_to_env])]
+                "PreToolUse": [HookMatcher(matcher="Read|Edit", hooks=[restrict_access_to_env])],
+                "PostToolUse": [HookMatcher(matcher="Read|Edit", hooks=[say_goodbye])]
             }
         )
     ):

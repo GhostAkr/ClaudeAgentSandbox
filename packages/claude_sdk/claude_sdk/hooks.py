9e77b852ecbc58ef30b882c0fb170bc6f7be6591
Alexander Antoshkin <ighost99@gmail.com>
2026-08-26 21:43:06 +0200

    feat: add a PreToolUse hook

---
+++ packages/claude_sdk/claude_sdk/hooks.py
@@ -0,0 +1,14 @@
+async def restrict_access_to_env(input_data: object, tool_use_id, context) -> object:
+    file_path: str = input_data["tool_input"].get("file_path", "")
+    file_name = file_path.split("/")[-1]
+
+    if file_name == ".env":
+        return {
+            "hookSpecificOutput": {
+                "hookEventName": input_data["hook_event_name"],
+                "permissionDecision": "deny",
+                "permissionDecisionReason": "Cannot modify .env files",
+            }
+        }
+
+    return {}
--- packages/claude_sdk/claude_sdk/main_agent.py
+++ packages/claude_sdk/claude_sdk/main_agent.py
@@ -1,17 +1,24 @@
-import os
-
 from claude_agent_sdk import (AssistantMessage, ClaudeAgentOptions,
-                              ResultMessage, query)
+                              HookMatcher, ResultMessage, query)
+from claude_sdk.hooks import restrict_access_to_env
 from claude_sdk.subagents import subagents
 from dotenv import load_dotenv
 
 load_dotenv()
 
+files_to_read: list(str) = [
+    "/Users/ghostakr/Repo/ClaudeAgentSandbox/data/example.txt",
+    "/Users/ghostakr/Repo/ClaudeAgentSandbox/.env"
+]
+
 async def run_coordinator():
     async for message in query(
-        prompt="Read /Users/ghostakr/Repo/ClaudeAgentSandbox/data/example.txt and summarize its contents.",
+        prompt=f"Read {files_to_read[1]} and summarize its contents.",
         options=ClaudeAgentOptions(
-            agents=subagents
+            # agents=subagents,  # Uncomment if you want to test subagents. Note that it might conflict with the hooks
+            hooks={
+                "PreToolUse": [HookMatcher(matcher="Read|Edit", hooks=[restrict_access_to_env])]
+            }
         )
     ):
         print("==================")
